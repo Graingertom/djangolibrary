e261a15710fb425f1709e628bfd3bae35301a5b8
isabel <isargp@gmail.com>
2022-04-27 15:47:56 +0100

    models created, migrate done

--- ourProject/library/models.py
+++ ourProject/library/models.py
@@ -1,3 +1,18 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Author(models.Model):
+   name = models.CharField(max_length=50)
+   
+   def __str__(self):
+       return self.name
+
+class Book(models.Model):
+   title = models.CharField(max_length=100)
+   author = models.ForeignKey(Author, on_delete=models.CASCADE, null=False)
+   
+   def __str__(self):
+       return f"{self.title} ({self.author.name})"
+
